76da68074a029c430768bf491f37daa564962630
Aleadra Amor <aamor@sandiego.edu>
2024-03-07 10:44:46 -0800

    q

--- custom_pentagon.py
+++ custom_pentagon.py
@@ -8,20 +8,37 @@ import turtle
 
 # create a turtle and set the pen color
 sat = turtle.Turtle()
-sat.pencolor("red")
+sat.pencolor(input("What pen color?"))
+
+sun = turtle.Turtle()
+sun.pencolor("green")
+
 
 # draw the pentagon
-sat.forward(50)
+sat.forward(150)
 sat.left(72)
-sat.forward(50)
+sat.forward(150)
 sat.left(72)
-sat.forward(50)
+sat.forward(150)
 sat.left(72)
-sat.forward(50)
+sat.forward(150)
 sat.left(72)
-sat.forward(50)
+sat.forward(150)
 sat.left(72)
 
+#draw smaller pentagon
+sun.up()
+sun.goto(5,5) #move smaller pentagon so lines dont overlap
+sun.down()
+sun.forward(25)
+sun.left(72)
+sun.forward(25)
+sun.left(72)
+sun.forward(25)
+sun.left(72)
+sun.forward(25)
+sun.left(72)
+sun.forward(25)
 # keep the turtle window open until we click on it
 turtle_window = turtle.Screen()
 turtle_window.exitonclick()
