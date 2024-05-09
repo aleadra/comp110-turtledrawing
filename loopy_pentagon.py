0a9b32b2b600353e38cc858dc286c183dc2bdac2
Aleadra Amor <aamor@sandiego.edu>
2024-05-08 18:28:11 -0700

    l

--- loopy_pentagon.py
+++ loopy_pentagon.py
@@ -10,17 +10,10 @@ import turtle
 sat = turtle.Turtle()
 sat.pencolor("red")
 
+for item in range(6):
+    sat.forward(50)
+    sat.left(72)
 # draw the pentagon
-sat.forward(50)
-sat.left(72)
-sat.forward(50)
-sat.left(72)
-sat.forward(50)
-sat.left(72)
-sat.forward(50)
-sat.left(72)
-sat.forward(50)
-sat.left(72)
 
 # keep the turtle window open until we click on it
 turtle_window = turtle.Screen()
